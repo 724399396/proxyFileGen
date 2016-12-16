2107db866e5cad4d2e5026ee68e0746981f5a9bb
liww.li <liww.li@qunar.com>
2016-12-16 13:32:25 +0800

    first version

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "proxyFileGen"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies ++= Seq(
+  "org.scalaj" %% "scalaj-http" % "2.3.0",
+  "com.typesafe" % "config" % "1.3.1"
+)
---
+++ src/main/scala-2.12/Main.scala
@@ -0,0 +1,31 @@
+import java.io.PrintWriter
+import java.util.Base64
+
+import com.typesafe.config.ConfigFactory
+
+import scala.io.Source
+import scalaj.http.Http
+
+object Main extends App {
+  val conf = ConfigFactory.load()
+  val url = conf.getString("gfwUrl")
+  val proxyHost = conf.getString("proxyHost")
+  val proxyPort = conf.getInt("proxyPort")
+  val gfwBase64 = Http(url).proxy(proxyHost, proxyPort).asString.body
+  val gfw = new String(Base64.getMimeDecoder.decode(gfwBase64))
+  println(gfw)
+  val removeComment = gfw.split("\n").drop(1).takeWhile(!_.contains("Whitelist")).toList.filter(!_.startsWith("!"))
+    .map(_.dropWhile(x => x == '|' || x == '@' || x == '.'))
+    .filter(!_.trim.isEmpty)
+
+  val pacTemplate = Source.fromInputStream(getClass.getResourceAsStream("pacFileTemplate.txt")).getLines().mkString("\n")
+
+  val out = new PrintWriter("/home/work/person_env_setting/myproxy.pac")
+  out.print("var domains = {")
+  removeComment.foreach{ x =>
+    out.println(s""" "$x": 1,""")
+  }
+  out.println("}")
+  out.print(pacTemplate)
+  out.close()
+}
