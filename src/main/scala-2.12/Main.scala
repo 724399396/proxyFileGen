fae03af3431c41eebd432f292564b7cb92f9c4f1
liwei <liwei72439936@gmail.com>
2017-07-31 22:38:59 +0800

    add munual config url list

--- src/main/scala-2.12/Main.scala
+++ src/main/scala-2.12/Main.scala
@@ -11,18 +11,23 @@ object Main extends App {
   val url = conf.getString("gfwUrl")
   val proxyHost = conf.getString("proxyHost")
   val proxyPort = conf.getInt("proxyPort")
-  val gfwBase64 = Http(url).proxy(proxyHost, proxyPort).asString.body
+  val gfwBase64 = Http(url).proxy(proxyHost, proxyPort, java.net.Proxy.Type.SOCKS).asString.body
   val gfw = new String(Base64.getMimeDecoder.decode(gfwBase64))
   println(gfw)
   val removeComment = gfw.split("\n").drop(1).takeWhile(!_.contains("Whitelist")).toList.filter(!_.startsWith("!"))
     .map(_.dropWhile(x => x == '|' || x == '@' || x == '.'))
     .filter(!_.trim.isEmpty)
 
+  val additionalUrl = Source.fromInputStream(getClass.getResourceAsStream("additional-proxy-urls.txt")).getLines()
+
   val pacTemplate = Source.fromInputStream(getClass.getResourceAsStream("pacFileTemplate.txt")).getLines().mkString("\n")
 
   val out = new PrintWriter("/home/work/person_env_setting/myproxy.pac")
   out.print("var domains = {")
   removeComment.foreach{ x =>
+    out.println(s""" "$x": 1,""")
+  }
+  additionalUrl.foreach{ x =>
     out.println(s""" "$x": 1,""")
   }
   out.println("}")
